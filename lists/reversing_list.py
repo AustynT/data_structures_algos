6ba7180262509d89c7b8e7ad548f5ac00bc5b5e3
AustynT <AustynTravis9@gmail.com>
2024-12-01 17:26:49 -0700

    found better way to reverse list

--- lists/reversing_list.py
+++ lists/reversing_list.py
@@ -1,3 +1,6 @@
+from pprint import pprint
+
+
 test_lists = [
     ([1, 2, 3, 4, 5], [5, 4, 3, 2, 1]),
     ([-5, -10, -15, -20], [-20, -15, -10, -5]),
@@ -10,17 +13,21 @@ test_lists = [
     ([1, "two", 3.0, [4, 5]], [[4, 5], 3.0, "two", 1]),
     (list(range(1, 101)), list(range(100, 0, -1))),
 ]
-
+pprint(list(enumerate(test_lists)))
 # my first attempt at list reversing
 def reverse_list(lst):
-    # Your code here
-    print(lst)
+    # Your code here  
     count = len(lst) - 1 
     new_list = []
     while (count >= 0):
         new_list.append(lst[count])        
         count -= 1
     return new_list
+
+
+## Optimized implementation in at the C level in python.
+def reverse_list_splice(test_lists):
+    return test_lists[::1]
     
     
 
